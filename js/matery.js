6634773623c6a96c3c89f0a382b93321ab971556
ShakeJ96 <919052005@qq.com>
2023-07-06 22:56:42 +0800

    Site updated: 2023-07-06 22:56:41

--- js/matery.js
+++ js/matery.js
@@ -40,13 +40,15 @@ $(function () {
      */
     let fixFooterPosition = function () {
         $('.content').css('min-height', window.innerHeight - 165);
+        // $('.hbe-input-container').css('min-height', window.innerHeight - 165);
+
     };
 
     /**
      * 修复样式.
      */
     let fixStyles = function () {
-        fixPostCardWidth('navContainer');
+        fixPostCardWidth('navContainer', 'articles');
         fixPostCardWidth('artDetail', 'prenext-posts');
         fixFooterPosition();
     };
@@ -72,7 +74,7 @@ $(function () {
     let articleInit = function () {
         $('#articleContent a').attr('target', '_blank');
 
-        $('#articleContent img').each(function () {
+        $('#articleContent img').not('img.emoji').each(function () {
             let imgPath = $(this).attr('src');
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
             // 图片添加阴影
@@ -100,11 +102,21 @@ $(function () {
                 this.insertAdjacentElement('afterend', captionDiv)
             }
         });
+
         $('#articleContent, #myGallery').lightGallery({
             selector: '.img-item',
             // 启用字幕
-            subHtmlSelectorRelative: true
+            subHtmlSelectorRelative: true,
+            showThumbByDefault: false  //2018.08.14
         });
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
+        
+        /*网站加载逻辑问题 网站加载逻辑问题：图片最后加载 洪卫 shw2018 modify 修改时间：2019.08.14*/
+        // $(document).find('img[data-original]').each(function () {
+        //     $(this).parent().attr("href", $(this).attr("data-original"));
+        // });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
@@ -120,36 +132,31 @@ $(function () {
 
     /*回到顶部*/
     $('#backTop').click(function () {
-        $('body,html').animate({scrollTop: 0}, 400);
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
         return false;
     });
 
     /*监听滚动条位置*/
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
-    // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
-    showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
         /* 回到顶部按钮根据滚动条的位置的显示和隐藏.*/
         let scroll = $(window).scrollTop();
-        showOrHideNavBg(scroll);
-    });
-
-    function showOrHideNavBg(position) {
-        let showPosition = 100;
-        if (position < showPosition) {
+        if (scroll < 100) {
             $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
         } else {
             $nav.removeClass('nav-transparent');
             $backTop.slideDown(300);
         }
-    }
+    });
 
-    	
-	$(".nav-menu>li").hover(function(){
+    // 增加二级菜单功能 洪卫 shw2018 add 2019.09.17
+    $(".nav-menu>li").hover(function(){
 		$(this).children('ul').stop(true,true).show();
-		 $(this).addClass('nav-show').siblings('li').removeClass('nav-show');
+		$(this).addClass('nav-show').siblings('li').removeClass('nav-show');
 		
 	},function(){
 		$(this).children('ul').stop(true,true).hide();
@@ -159,14 +166,12 @@ $(function () {
     $('.m-nav-item>a').on('click',function(){
             if ($(this).next('ul').css('display') == "none") {
                 $('.m-nav-item').children('ul').slideUp(300);
-                $(this).next('ul').slideDown(100);
+                $(this).next('ul').slideDown(300);
                 $(this).parent('li').addClass('m-nav-show').siblings('li').removeClass('m-nav-show');
             }else{
-                $(this).next('ul').slideUp(100);
+                $(this).next('ul').slideUp(300);
                 $('.m-nav-item.m-nav-show').removeClass('m-nav-show');
             }
     });
 
-    // 初始化加载 tooltipped.
-    $('.tooltipped').tooltip();
 });
